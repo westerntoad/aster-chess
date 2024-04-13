d41ede5c896791eb314065a9dbcd98045912fc67
westerntoad <abr.engebretson@gmail.com>
2024-04-12 19:36:28 -0700

    Add knight move gen

--- src/legal_moves.rs
+++ src/legal_moves.rs
@@ -1,11 +1,39 @@
 
 use crate::bitboard::Bitboard;
 
-pub fn king_move_gen(bb: Bitboard) -> Bitboard {
-    let output: Bitboard = bb.nort_one() | bb.sout_one();
-    output | output.west_one() | output.east_one() | bb.west_one() | bb.east_one()
+
+pub fn pawn_move_gen(orig: Bitboard) -> Bitboard {
+    todo!();
 }
 
+pub fn n_move_gen(orig: Bitboard) -> Bitboard {
+    let mut horizontal = orig.east_one().east_one();
+    horizontal |= orig.west_one().west_one();
+    horizontal = horizontal.nort_one() | horizontal.sout_one();
+
+    let mut vertical = orig.nort_one().nort_one();
+    vertical |= orig.sout_one().sout_one();
+    vertical = vertical.east_one() | vertical.west_one();
+
+    horizontal | vertical
+}
+
+pub fn b_move_gen(orig: Bitboard) -> Bitboard {
+    todo!();
+}
+
+pub fn r_move_gen(orig: Bitboard) -> Bitboard {
+    todo!();
+}
+
+pub fn q_move_gen(orig: Bitboard) -> Bitboard {
+    todo!();
+}
+
+pub fn k_move_gen(orig: Bitboard) -> Bitboard {
+    let attacks = orig.nort_one() | orig.sout_one();
+    attacks | attacks.west_one() | attacks.east_one() | orig.west_one() | orig.east_one()
+}
 
 #[cfg(test)]
 mod tests {
@@ -13,8 +41,8 @@ mod tests {
     use crate::square::*;
 
     #[test]
-    fn test_king_move_gen() {
-        let output = king_move_gen(Square::E2.bb());
+    fn test_k_move_gen() {
+        let output = k_move_gen(Square::E2.bb());
         let expected_output = Bitboard::new(0x3828380000000000);
 
 
@@ -22,16 +50,16 @@ mod tests {
     }
 
     #[test]
-    fn test_king_move_gen_edge() {
-        let output = king_move_gen(Square::A4.bb());
+    fn test_k_move_gen_edge() {
+        let output = k_move_gen(Square::A4.bb());
         let expected_output = Bitboard::new(0x00030203000000);
 
         assert_eq!(output, expected_output);
     }
 
     #[test]
-    fn test_king_move_gen_corner() {
-        let output = king_move_gen(Square::H8.bb());
+    fn test_k_move_gen_corner() {
+        let output = k_move_gen(Square::H8.bb());
         let expected_output = Bitboard::new(0x0000000000c040);
 
         assert_eq!(output, expected_output);
